ae5c42809daf7b93c7ee4723ab13be37633aee26
unknown <superhuisama@foxmail.com>
2020-04-05 17:16:28 +0800

    ch03

---
+++ ch03/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: string;
+  imgUrl: string;
+  url: string;
+
+  constructor() {
+    this.title = '123';
+    this.imgUrl = '/assets/imgs/ng-book2.png';
+    this.url = 'https://www.baidu.com';
+  }
+  onBtnClicked() {
+    console.log('hhhh');
+  }
+  OnInput(evt: Event) {
+    this.title = (<HTMLInputElement>evt.target).value;
+  }
+}
---
+++ ch03/src/app/student-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'student-list',
+    template: `
+    <student *ngFor='let name of studentNames' [name]='name' (studentSelected)="onStudentSelected($event)"></student>
+    <!-- <student [name]="studentNames[0]"></student> -->
+    `
+})
+export class StudentList {
+    studentNames = ['张三', '李四', '王五'];
+    constructor() {
+
+    }
+    onStudentSelected(name: string) {
+        console.log('选中的学生：' + name);
+    }
+}
